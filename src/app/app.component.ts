c671081bf3e606abd7683c8ac7a753028f0107e9
cwiktor / s21291 <wiktorczech811@gmail.com>
2023-03-07 10:30:50 +0100

    updated gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -74,25 +74,22 @@ export class AppComponent implements OnInit {
     }
   }
 
-  /*
-    public searchEmployees(key: string): void {
-      console.log(key);
-      const results: Employee[] = [];
-      for (const employee of this.employees) {
-        if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
-          || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
-          || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
-          || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
-          results.push(employee);
-        }
-      }
-      this.employees = results;
-      if (results.length === 0 || !key) {
-        this.getEmployees();
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
       }
     }
-  
-    */
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
 
   public onOpenModal(employee: Employee | null, mode: string): void {
     const container = document.getElementById('main-container');
